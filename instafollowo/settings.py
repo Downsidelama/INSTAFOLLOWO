403714a7285b5bbd9ec14a80e6e8855511c7bbb3
Downsidelama <27288245+Downsidelama@users.noreply.github.com>
2019-03-05 14:57:36 +0100

    Adding login page.
    Adding dashboard app.

--- instafollowo/settings.py
+++ instafollowo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'instafollowo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
--- instafollowo/urls.py
+++ instafollowo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('authentication/', include('authentication.urls')),
+    path('authentication/', include('django.contrib.auth.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
