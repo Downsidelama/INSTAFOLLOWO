5f16faa80e5d3c51ddd2aa984eff71c18e29b1a6
John Doe <27288245+Downsidelama@users.noreply.github.com>
2019-03-20 18:47:09 +0100

    Creating model for instagram account

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class InstagramAccount(models.Model):
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
+    started = models.BooleanField()
+    run_type = models.CharField(max_length=20)
+    hashtag = models.CharField(max_length=150)
+    other_profile = models.CharField(max_length=150)
