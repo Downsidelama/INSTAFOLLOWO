26e81984e897cd3876f374ab9ee77e5b24ea3f05
John Doe <27288245+Downsidelama@users.noreply.github.com>
2019-03-21 16:11:26 +0100

    Adding account listing page
    Created profile page, doesn't contain anything yet.
    Updated InstagramAccount model.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'dashboard'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('accounts/', views.accounts, name='accounts'),
+    path('accounts/new/', views.add_account, name='add_account'),
+    path('', views.profile, name='profile')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
+from .models import InstagramAccount
 
-# Create your views here.
 
+@login_required
 def index(request):
     return render(request, 'dashboard/index.html')
+
+
+@login_required
+def accounts(request):
+    context = {
+        'accounts': list(InstagramAccount.objects.filter(user_id=request.user.id)),
+        'account_count': InstagramAccount.objects.filter(user_id=request.user.id).count(),
+    }
+    return render(request, 'dashboard/accounts.html', context)
+
+
+@login_required
+def profile(request):
+    return render(request, 'dashboard/profile.html')
