f1551e9a9b174754fab3a533565ff2472e145dd5
Janos Hrubos <janoshrubos@icloud.com>
2019-04-11 09:42:20 +0200

    working login and registration

--- instafollowo/settings.py
+++ instafollowo/settings.py
@@ -124,6 +124,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = '/'
