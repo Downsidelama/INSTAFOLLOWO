c6ed5bd99d5b2858442a89549fc5e393972e42b2
Downsidelama <27288245+Downsidelama@users.noreply.github.com>
2019-04-24 20:36:24 +0200

    Fixing issues that CodeFactor pointed out

--- bot/bot.py
+++ bot/bot.py
@@ -4,87 +4,84 @@ import re
 import time
 import json
 
-#teszt fiok:
-#gmail.hu instafollowo pe1234
-#instagram instfow1 pe1234
+
+# teszt fiok:
+# gmail.hu instafollowo pe1234
+# instagram instfow1 pe1234
 
 class Bot:
-
-    #Urls
+    # Urls
     url_base = "https://www.instagram.com"
 
     url_login = url_base + "/accounts/login/ajax/"
     url_logout = url_base + "/accounts/logout/"
 
-    #username
+    # username
     url_profile = url_base + "/{}/"
 
-    #userid
+    # userid
     url_follow = url_base + "/web/friendships/{}/follow/"
     url_unfollow = url_base + "/web/friendships/{}/unfollow/"
 
     url_api = "https://i.instagram.com/api/v1/users/{}/info/"
 
-    #username
+    # username
     url_profile = url_base + "/{}"
     url_profile_json = url_profile + "/?__a=1"
 
-    #mediaid
+    # mediaid
     url_like = url_base + "/web/likes/{}/like/"
     url_unlike = url_base + "/web/likes/{}/unlike/"
 
-    #tag
-    #maxid?
+    # tag
+    # maxid?
     url_media_by_tag = url_base + "/explore/tags/{}/?__a=1"
     url_media_by_tag_maxid = url_base + "/explore/tags/{}/?__a=1&max_id={}/"
 
-    #userid, first(amount)
-    #after?
+    # userid, first(amount)
+    # after?
     url_following = url_base + "/graphql/query/?query_id=17874545323001329&id={}&first={}"
     url_following_after = url_base + "/graphql/query/?query_id=17874545323001329&id={}&first={}&after={}"
 
     url_followers = url_base + "/graphql/query/?query_id=17851374694183129&id={}&first={}"
     url_followers_after = url_base + "/graphql/query/?query_id=17851374694183129&id={}&first={}&after={}"
 
-
-    #Fake headers
-    #TODO: add fake_useragent maybe
+    # Fake headers
+    # TODO: add fake_useragent maybe
     headers_list = [
-        "Mozilla/5.0 (Windows NT 5.1; rv:41.0) Gecko/20100101"\
+        "Mozilla/5.0 (Windows NT 5.1; rv:41.0) Gecko/20100101" \
         " Firefox/41.0",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2)"\
-        " AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2"\
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2)" \
+        " AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2" \
         " Safari/601.3.9",
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0)"\
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0)" \
         " Gecko/20100101 Firefox/15.0.1",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"\
-        " (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"\
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36" \
+        " (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36" \
         " Edge/12.246"
     ]
-
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.userid = 0
-        self.user_agent = self.headers_list[random.randrange(0,4)]
+        self.user_id = 0
+        self.user_agent = self.headers_list[random.randrange(0, 4)]
 
         self.loggedin = False
         self.session = requests.Session()
 
-        #self.followercount = 0
-        #self.followingcount = 0
+        # self.followercount = 0
+        # self.followingcount = 0
 
         self.login()
 
-
-    #TODO: more exception checks
+    # TODO: more exception checks
     def login(self):
 
         self.session.headers.update(
             {
-            "Referer": self.url_base,
-            "User-Agent": self.user_agent
+                "Referer": self.url_base,
+                "User-Agent": self.user_agent
             }
         )
 
@@ -97,7 +94,7 @@ class Bot:
         csrf_token = re.search('(?<="csrf_token":")\w+', info.text).group(0)
         self.session.headers.update({"X-CSRFToken": csrf_token})
 
-        login = self.session.post(self.url_login, data = self.login_data, allow_redirects = True)
+        login = self.session.post(self.url_login, data=self.login_data, allow_redirects=True)
         try:
             self.session.headers.update({"X-CSRFToken": login.cookies["csrftoken"]})
             self.csrftoken = login.cookies["csrftoken"]
@@ -110,7 +107,7 @@ class Bot:
             print("Successful login!")
             self.loggedin = True
             self.session.cookies["csrftoken"] = csrf_token
-            self.userid = self.get_userid_by_username(self.username)
+            self.user_id = self.get_userid_by_username(self.username)
         else:
             print("Login error")
 
@@ -123,7 +120,6 @@ class Bot:
         else:
             print("Not logged in")
 
-    
     def get_userdata(self, username):
         url_profile = self.url_profile_json.format(username)
 
@@ -139,7 +135,7 @@ class Bot:
             follow = self.session.post(url_follow)
 
             if follow.status_code == 200:
-                #self.followercount += 1
+                # self.followercount += 1
                 print("Followed " + userid)
 
     def unfollow(self, userid):
@@ -149,7 +145,7 @@ class Bot:
             unfollow = self.session.post(url_unfollow)
 
             if unfollow.status_code == 200:
-                #self.followercount -= 1
+                # self.followercount -= 1
                 print("Unfollowed " + userid)
 
     def like(self, mediaid):
@@ -173,12 +169,11 @@ class Bot:
         follows_viewer = user_info["follows_viewer"]
         has_requested_viewer = user_info["has_requested_viewer"]
 
-        if (follows_viewer or has_requested_viewer):
+        if follows_viewer or has_requested_viewer:
             print("Followed by " + username)
             return True
-        else:
-            print("Not followed by " + username)
-            return False
+        print("Not followed by " + username)
+        return False
 
     def is_following(self, username):
         # checks if active profile follows target profile
@@ -187,12 +182,11 @@ class Bot:
         followed_by_viewer = user_info["followed_by_viewer"]
         requested_by_viewer = user_info["requested_by_viewer"]
 
-        if (followed_by_viewer or requested_by_viewer):
+        if followed_by_viewer or requested_by_viewer:
             print("Following " + username)
             return True
-        else:
-            print("Not following " + username)
-            return False
+        print("Not following " + username)
+        return False
 
     def get_username_by_userid(self, userid):
         # this link may get deprecated 
@@ -216,75 +210,82 @@ class Bot:
         if self.loggedin:
             i = self.session.get(url_media_by_tag)
             media_data = json.loads(i.text)["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"]
-            for i in range(len(media_data)):
-                mediaids_by_tag.append((media_data[i]["node"]["id"]))
+            for media in media_data:
+                mediaids_by_tag.append(media["node"]["id"])
 
         return mediaids_by_tag
 
     def get_userids_by_tag(self, tag):
-        # returns around 70 userids by a hashtag
+        """
+        returns around 70 userids by a hashtag
+        """
         url_media_by_tag = self.url_media_by_tag.format(tag)
         userids_by_tag = []
 
         if self.loggedin:
             i = self.session.get(url_media_by_tag)
             media_data = json.loads(i.text)["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"]
-            for i in range(len(media_data)):
-                userids_by_tag.append((media_data[i]["node"]["owner"]["id"]))
+            for media in media_data:
+                userids_by_tag.append(media["node"]["owner"]["id"])
 
         return userids_by_tag
 
-    def get_followerids(self, userid, amount = 50):
-        # returns a maximum of 50 userids that follow target profile
-        # seems to actually return less than 50 in most cases for some reason
-        # rate_limited error occurs after around 20 requests but resumes inside ~30 mins(?)
-        url_followers = self.url_followers.format(userid, amount)
+    def get_followerids(self, user_id, amount=50):
+        """
+        returns a maximum of 50 userids that follow target profile
+        seems to actually return less than 50 in most cases for some reason
+        rate_limited error occurs after around 20 requests but resumes inside ~30 mins(?)
+        """
+
+        url_followers = self.url_followers.format(user_id, amount)
         follower_ids = []
         info = self.session.get(url_followers)
         try:
             followers_data = json.loads(info.text)["data"]["user"]["edge_followed_by"]["edges"]
-            for i in range(len(followers_data)):
-                follower_ids.append(followers_data[i]["node"]["id"])
+            for follower_data in followers_data:
+                follower_ids.append(follower_data["node"]["id"])
         except KeyError:
             print("ERROR: " + json.loads(info.text)["message"])
 
         return follower_ids
-    
-    def get_followingids(self, userid, amount = 50):
-        # returns userids that target profile is following
+
+    def get_followingids(self, userid, amount=50):
+        """
+        returns userids that target profile is following
+        """
         url_followings = self.url_following.format(userid, amount)
         following_ids = []
         info = self.session.get(url_followings)
         try:
             following_data = json.loads(info.text)["data"]["user"]["edge_follow"]["edges"]
-            for i in range(len(following_data)):
-                following_ids.append(following_data[i]["node"]["id"])
+            for data in following_data:
+                following_ids.append(data["node"]["id"])
         except KeyError:
             print("ERROR: " + json.loads(info.text)["message"])
 
-        return following_ids 
+        return following_ids
 
     def unfollow_if_no_followback(self):
         # unfollows up to 50 profiles that dont follow active profile
-        userids = self.get_follower_ids(self.userid)
-        for i in range(len(userids)):
-            if not self.is_followed_by(userids[i]):
-                self.unfollow(userids[i])
+        user_ids = self.get_follower_ids(self.user_id)
+        for user_id in user_ids:
+            if not self.is_followed_by(user_id):
+                self.unfollow(user_id)
 
     def follow_by_tag(self, tag):
         # TODO: add limits to not get detected as a bot
-        userids = self.get_userids_by_tag(tag)
-        for i in range(len(userids)):
-            self.follow(userids[i])
+        user_ids = self.get_userids_by_tag(tag)
+        for user_id in user_ids:
+            self.follow(user_id)
 
-    def follow_by_user_followers(self, userid):
+    def follow_by_user_followers(self, user_id):
         # TODO: add limits to not get detected as a bot
-        userids = self.get_followerids(userid)
-        for i in range(len(userids)):
-            self.follow(userids[i])
+        follower_ids = self.get_followerids(user_id)
+        for follower_id in follower_ids:
+            self.follow(follower_id)
 
     def like_by_tag(self, tag):
         # TODO: add limits to not get detected as a bot
-        mediaids = self.get_mediaids_by_tag(tag)
-        for i in range(len(mediaids)):
-            self.like(mediaids[i])
+        media_ids = self.get_mediaids_by_tag(tag)
+        for media_id in media_ids:
+            self.like(media_id)
--- bot/tests.py
+++ bot/tests.py
@@ -3,20 +3,21 @@ from .bot import Bot
 
 import time
 
+
 class BotTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.username = "instfow1"
         cls.password = "pe1234"
         cls.bot = Bot(cls.username, cls.password)
-    
+
     def setUp(self):
         if not self.bot.loggedin:
             self.bot.login()
 
     def test_logged_in(self):
         self.assertTrue(self.bot.loggedin)
-    
+
     def test_false_login(self):
         bot = Bot("instfow1", "pe12345")
         self.assertFalse(bot.loggedin)
@@ -24,7 +25,7 @@ class BotTestCase(TestCase):
     def test_logout(self):
         self.bot.logout()
         self.assertFalse(self.bot.loggedin)
-        
+
     def test_get_userdata(self):
         profile = "natgeo"
         username = self.bot.get_userdata(profile)["username"]
@@ -34,15 +35,15 @@ class BotTestCase(TestCase):
         profile = "natgeo"
         userid = self.bot.get_userid_by_username(self.username)
         userid2 = self.bot.get_userid_by_username(profile)
-        self.assertEquals((userid, userid2), (self.bot.userid, "787132"))
+        self.assertEquals((userid, userid2), (self.bot.user_id, "787132"))
 
     def test_get_username_by_userid(self):
-        username = self.bot.get_username_by_userid(self.bot.userid)
+        username = self.bot.get_username_by_userid(self.bot.user_id)
         username2 = self.bot.get_username_by_userid("787132")
         self.assertEquals((username, username2), (self.username, "natgeo"))
 
     def test_is_followed_by(self):
-        profile_true = "placeholder" #none:(
+        profile_true = "placeholder"  # none:(
         profile_false = "natgeo"
         bool_true = self.bot.is_followed_by(profile_true)
         bool_false = self.bot.is_followed_by(profile_false)
@@ -63,7 +64,7 @@ class BotTestCase(TestCase):
             time.sleep(3)
         self.bot.follow(userid)
         self.assertTrue(self.bot.is_following(profile))
-    
+
     def test_unfollow(self):
         profile = "natgeowild"
         userid = self.bot.get_userid_by_username(profile)
@@ -84,16 +85,13 @@ class BotTestCase(TestCase):
         self.assertTrue(len(array) > 10)
 
     def test_get_followerids(self):
-        profile = "natgeo" #must have atleast 1 follower
+        profile = "natgeo"  # must have atleast 1 follower
         profileid = self.bot.get_userid_by_username(profile)
         userid = self.bot.get_followerids(profileid, 1)
         self.assertEquals(len(userid), 1)
 
     def test_get_followingids(self):
-        profile = "natgeo" #must have atleast 1 following
+        profile = "natgeo"  # must have atleast 1 following
         profileid = self.bot.get_userid_by_username(profile)
         userid = self.bot.get_followingids(profileid, 1)
         self.assertEquals(len(userid), 1)
-
-
-    
